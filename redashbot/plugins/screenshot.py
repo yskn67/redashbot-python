5db99dec6aa98b10cd1447e35a14f28d300a46c8
yskn67 <yskn1967@gmail.com>
2018-04-28 01:00:20 +0900

    Resolve #9

--- redashbot/plugins/screenshot.py
+++ redashbot/plugins/screenshot.py
@@ -3,6 +3,7 @@
 
 import os
 import tempfile
+from time import sleep
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from slackbot.bot import respond_to
@@ -29,6 +30,7 @@ def screenshot(msg, query_id, visual_id):
     driver = webdriver.Chrome(chrome_options=options)
     driver.set_window_size(720, 360)
     driver.get(embed_url)
+    sleep(1)
     driver.save_screenshot(fname)
     driver.quit()
 
